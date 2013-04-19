f21a894198d008cedbc08e2bda876f4c240e9633
yamapon <yasuakiya@gmail.com>
2013-04-19 19:17:44 +0900

    update

--- rmongosource.R
+++ rmongosource.R
@@ -3,9 +3,15 @@ library(rmongodb)
 mongo <- mongo.create(db = 'finance')
 
 buf <- mongo.bson.buffer.create()
-value <- c( "Prior1YearNonConsolidatedInstant", "DocumentInfo")
+value <- c( "33440", "CurrentYearConsolidatedInstant")
 names(value) <- c( "#text", "contextRef")
-mongo.bson.buffer.append.string(buf, "jpfr-di:ContextIDBeginningOfPeriodSCNonconsolidatedSS", value)
+mongo.bson.buffer.append.string(buf, "tse-t-ed:SecuritiesCode", value)
 b<-mongo.bson.from.buffer(buf)
 
-mongo.find.one(mongo,'finance.finance', b)
+#mongo.find(mongo,'finance.finance', b)
+#以下で対象の抽出（select A from B where B.c = '11' )が出来た
+lst <- list("tse-t-ed:ForecastOperatingIncome" = 1)
+c <- mongo.bson.from.list(lst)
+cur <- mongo.find(mongo,'finance.finance', query=b,fields=c)
+while( mongo.cursor.next(cur))
+  print(mongo.cursor.value(cur))
