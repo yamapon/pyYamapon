37dc21b77719213f00e7ab36082346ea79e6e619
yamapon <yasuakiya@gmail.com>
2013-04-25 19:12:00 +0900

    update

--- Rmongo2.R
+++ Rmongo2.R
@@ -6,10 +6,10 @@ if (mongo.is.connected(mongo)){
   buf <- mongo.bson.buffer.create()
   
   #完全一致での検索方法
-  mongo.bson.buffer.start.object(buf, "tse-t-ed:SecuritiesCode")
-  mongo.bson.buffer.append(buf, "#text", "37120")
-  mongo.bson.buffer.append(buf, "contextRef", "CurrentYearNonConsolidatedInstant")
-  mongo.bson.buffer.finish.object(buf)
+  #mongo.bson.buffer.start.object(buf, "tse-t-ed:SecuritiesCode")
+  #mongo.bson.buffer.append(buf, "#text", "37120")
+  #mongo.bson.buffer.append(buf, "contextRef", "CurrentYearNonConsolidatedInstant")
+  #mongo.bson.buffer.finish.object(buf)
   
   #nestされた項目の部分検索方法(サンプル：業績修正データを取得)
   regex <- mongo.regex.create("tdnet-rvfc.*" , options="i")
@@ -23,27 +23,38 @@ if (mongo.is.connected(mongo)){
   #カーソルの取得
   cur <- mongo.find(mongo,'finance.finance', query=b)
     
+  num <- 0
   while( mongo.cursor.next(cur)){
     result <- mongo.cursor.value(cur)
     doc <- mongo.bson.value(result, "link:schemaRef")
-    print(doc["xlink:href"])
     SecuritiesCode <- mongo.bson.value(result, "tse-t-ed:SecuritiesCode")
     ChangeNetSales <- mongo.bson.value(result, "tse-t-rv:ChangeNetSales")
     ChangeOperatingIncome <- mongo.bson.value(result, "tse-t-rv:ChangeOperatingIncome")
     ChangeOrdinaryIncome <- mongo.bson.value(result, "tse-t-rv:ChangeOrdinaryIncome")
     ChangeNetIncome <- mongo.bson.value(result, "tse-t-rv:ChangeNetIncome")
   
-    #結果をファイルに出力
-    #sink("C:/json.txt")
-    #print(result) 
-    #sink() 
-
     #dataframeに挿入する(NULLの場合を考慮する必要あり)
-    df <- as.data.frame( list("SecuritiesCode" = SecuritiesCode["#text"], 
-                              "ChangeNetSales" = ChangeNetSales["#text"], 
-                              "ChangeOperatingIncome" = ChangeOperatingIncome["#text"], 
-                              "ChangeOrdinaryIncome" = ChangeOrdinaryIncome["#text"], 
-                              "ChangeNetIncome" = ChangeNetIncome["#text"]))
+    if( is.null(ChangeNetSales) == FALSE 
+        && is.null(ChangeOperatingIncome) == FALSE
+        && is.null(ChangeOrdinaryIncome) == FALSE
+        && is.null(ChangeNetIncome) == FALSE){
+      
+        df <- as.data.frame( list("SecuritiesCode" = SecuritiesCode, 
+                                "ChangeNetSales" = ChangeNetSales, 
+                                "ChangeOperatingIncome" = ChangeOperatingIncome, 
+                                "ChangeOrdinaryIncome" = ChangeOrdinaryIncome, 
+                                "ChangeNetIncome" = ChangeNetIncome))
+              
+    }else{
+      #NG内容のものを
+#      sink(paste("C:/", doc["xlink:href"], ".txt"))
+#      print(result) 
+#      sink() 
+      
+      num <- num + 1
+    }
+    
   }
+  print(num)
   mongo.cursor.destroy(cur)
 }
